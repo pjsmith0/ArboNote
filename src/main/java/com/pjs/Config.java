d44cfc642ba91e10ead6fedad800b964c0efc532
PJ Smith <pjs@gmail.com>
2026-03-22 21:45:44 +0100

    Fixed user home

--- src/main/java/com/pjs/Config.java
+++ src/main/java/com/pjs/Config.java
@@ -2,13 +2,15 @@ package com.pjs;
 
 import lombok.Data;
 
+import java.nio.file.Path;
+
 @Data
 public class Config {
-    private String rootPath = "/home/pjs/jnotes_data";
+    private String rootPath = "arbonote_data";
     private String fileExtention = ".html";
     private String treeFileName = "tree.json";
 
-    public void init() {
-
+    public String getRootPath() {
+        return Path.of(System.getProperty("user.home"), rootPath).toString();
     }
 }
